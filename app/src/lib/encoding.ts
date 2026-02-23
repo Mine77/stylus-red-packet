3fcae423a2b2b7642897fc0fc1db87b9a81a9fd8
Mine77 <haichaozhu77@gmail.com>
2026-02-23 19:47:27 +0800

    add slides branch

--- app/src/lib/encoding.ts
+++
@@ -1,8 +0,0 @@
-export function toHex(data: ArrayBuffer | Uint8Array): `0x${string}` {
-  const bytes = data instanceof Uint8Array ? data : new Uint8Array(data);
-  let hex = "";
-  for (const byte of bytes) {
-    hex += byte.toString(16).padStart(2, "0");
-  }
-  return `0x${hex}` as `0x${string}`;
-}
